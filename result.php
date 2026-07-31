1c86cab5b94cfc20f1a5bc155e2c551b99e61bb6
mishty_30 <mishty.patel127756@marwadiuniversity.ac.in>
2026-07-31 11:28:47 +0530

    Add files via upload

--- result.php
+++ result.php
@@ -2,10 +2,10 @@
     echo "+---------------------------------------------+<br>";
     echo "|               MARWADI UNIVERSITY            |<br>";
     echo "+---------------------------------------------+<br>";
-    echo "|NAME : PATEL MISHTY                          |<br>";
+    echo "|NAME : SANGHANI GAURAV                       |<br>";
     echo "|SEM  : 4                                     |<br>";
-    echo "|GRNO : 127756                                |<br>";
-    echo "|ROLL NO. : 92400527199                       |<br>";
+    echo "|GRNO : 126725                                |<br>";
+    echo "|ROLL NO. : 92400527141                       |<br>";
     echo "+---------------------------------------------+<br>";
     echo "|C# : A++                                     |<br>";
     echo "|CN : A+                                      |<br>";
