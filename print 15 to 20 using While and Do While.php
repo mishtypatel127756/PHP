0c5861ce45e2306bd4af9e3cf5a4803a243974d0
mishty_30 <mishty.patel127756@marwadiuniversity.ac.in>
2026-07-31 11:30:34 +0530

    Add files via upload

---
+++ print 15 to 20 using While and Do While.php
@@ -0,0 +1,17 @@
+<?php
+    echo "Using While Loop:<br>";
+    
+    $a = 15;
+    while ($a <= 20) {
+        echo $a . "<br>";
+        $a++;
+    }
+
+    echo "<br>Using Do While Loop:<br>";
+    
+    $b = 15;
+    do {
+        echo $b . "<br>";
+        $b++;
+    } while ($b <= 20);
+?>
